8371065b39d713fd9367a74acd7c3e03c2e1a096
kir03 <pehenko.k@icloud.com>
2022-04-12 15:23:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = new int[10];
+for (int i = 0; i < numbers.Length; i++)
+{
+    int.TryParse(Console.ReadLine(), out numbers[i]);
+}
+int max = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        max = numbers[i];
+}
+Console.WriteLine($"Наибольшее число {max}");
